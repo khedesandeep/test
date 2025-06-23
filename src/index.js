5ff17621da2c6252abbc77006320b7b2d3e24600
Sandeep Khede <sandeep@webkorps.com>
2025-06-24 02:01:16 +0530

    chore: update dependencies

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import _ from "lodash";
+
+async function fetchData() {
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+    const data = _.pick(response.data, ["id", "title"]);
+    console.log("Fetched data:", data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+  }
+}
+
+fetchData();
